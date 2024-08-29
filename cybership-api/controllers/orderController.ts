0cbf265e7da62fc0235ba73d741610c275edbd96
vishakhavel <vichu2grand@gmail.com>
2024-08-28 19:24:45 -0700

    http best practices

--- cybership-api/controllers/orderController.ts
+++ cybership-api/controllers/orderController.ts
@@ -2,13 +2,24 @@ import { Request, Response } from "express";
 import prisma from "../src/prismaClient";
 
 export const getOrderData = async (req: Request, res: Response) => {
-  console.log("inside the getOrderData");
+  // printing the controller in square brackets for easier tracing when the API suite grows...
+  console.log("inside [controller] - getOrderData");
   try {
     const orders = await prisma.order.findMany();
-    console.log({ orders });
-    res.json(orders);
+    console.log(
+      "inside [controller] - getOrderData - orders fetched from postgres",
+      orders
+    );
+    res.status(200).json({
+      status: "success",
+      result: orders.length,
+      data: orders,
+    });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "An error occurred while fetching data." });
+    res.status(500).json({
+      status: "error",
+      message: "An error occurred while fetching data.",
+    });
   }
 };
--- cybership-api/routes/orderRoute.ts
+++ cybership-api/routes/orderRoute.ts
@@ -2,9 +2,6 @@ import { Router } from "express";
 import { getOrderData } from "../controllers/orderController";
 
 const router = Router();
-function check() {
-  console.log("inside the route");
-}
 router.get("/fulfillments", getOrderData);
 
 export default router;
